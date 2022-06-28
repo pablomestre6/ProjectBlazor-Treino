4e7834ddccc8ca6493332c5eb4043adf7f48134d
pablomestre6 <pablomestre6@gmail.com>
2022-06-28 19:09:33 -0300

    Add project files.

---
+++ ProjectBlazor/NewTester.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectBlazor
+{
+    public class NewTester
+    {
+        public string? Title { get; set; }
+        public bool Isdone { get; set; }
+    }
+}
---
+++ ProjectBlazor/Pages/Tester.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectBlazor.Pages
+{
+    public class TesterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
